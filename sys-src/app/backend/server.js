e35e813860956a1162e245f9d1fc5ef8443dc2df
Andreas Hecht <a.hecht1@oth-aw.de>
2024-06-19 20:49:08 +0200

    fixed GET endpoints

--- sys-src/app/backend/server.js
+++ sys-src/app/backend/server.js
@@ -7,48 +7,48 @@ const port =  3000;
 
 
 const pool = new Pool({
-    user: 'your_db_user', // Replace with DB user
-    host: 'your_db_host', // Replace with DB host
-    database: 'your_db_name', // Replace with DB name
-    password: 'your_db_password', // Replace with DB password
+    user: 'st_user', // Replace with DB user
+    host: 'host.docker.internal', // Replace with DB host
+    database: 'postgres', // Replace with DB name
+    password: '123', // Replace with DB password
     port: 5432,
 });
 
 
-app.get('/api/sentiments', async (req, res) => {
+app.get('/api/sentiments',async (req, res) => {
+
     const query =
-    "SELECT"+
-        "s.name AS stock_name," +
-        "s.ticker_symbol,"+
-        "sn.sentiment," +
-    "FROM" +
-        "stock s; "+
-    "JOIN"+
-        "stock_news sn ON s.stock_id = sn.stock_id;"
-    ;
+        "SELECT "+
+            " s.name,"+
+            " AVG(sn.sentiment) as AVG_Sentiment "+
+        " FROM "+
+            " stock s, stock_news sn "+
+        " WHERE s.stock_id = sn.stock_id AND EXTRACT('week' FROM sn.pub_date) = EXTRACT('week' FROM now()) "+
+        " group by s.name; "
 
     try {
         const result = await pool.query(query);
         res.status(200).json(result.rows);
     } catch (err) {
         console.error('Error executing query', err.stack);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500);
     }
 });
 
-app.get('/api/stockData', async (req, res) => {
+// GET like /api/stockData?stockName=(insert Stock name)
+app.get('/api/StockDataFor/:stockName', async (req, res) => {
     const query =
-        "SELECT"+
-            "s.name AS stock_name," +
-            "s.ticker_symbol,"+
-            "sp.stock_price_val," +
-            "sp.stock_price_time"+
-        "FROM" +
-            "stock s"+
-        "WHERE"+
-            "stock_name =" + String(req.query("stockName")) + // GET like /api/stockData?stockName=(insert Stock name)
-        "JOIN "+
-            "stock_price sp ON s.stock_id = sp.stock_id;"
+    "SELECT "+
+        "s.name AS stock_name, "+
+        "s.ticker_symbol, "+
+        "sp.stock_price_val, "+
+        "sp.stock_price_time "+
+    "FROM "+
+        "stock s, stock_price sp "+
+    "WHERE "+
+        "s.stock_id = sp.stock_id "+
+        "AND EXTRACT('week' FROM sp.stock_price_time) = EXTRACT('week' FROM now()) "+
+        "AND s.name = '" + String(req.params.stockName) + "';"
     ;
 
     try {
@@ -56,35 +56,29 @@ app.get('/api/stockData', async (req, res) => {
         res.status(200).json(result.rows);
     } catch (err) {
         console.error('Error executing query', err.stack);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500);
     }
 });
 
 
 // GET like /api/sentimentSources?sourceName=(insert source name)&?stockName=(insert stock name)
-app.get('/api/sentimentSources', async (req, res) => {
+app.get('/api/sentimentSources/:stockName', async (req, res) => {
     const query =
-        "SELECT"+
-            "sn.name AS source_name," +
-            "s.ticker_symbol," +
-            "s.name AS stock_name"+
-        "FROM" +
-            "stock_news sn"+
-        "WHERE"+
-            "source_name=" + String(req.query("sourceName")) +
-            "AND" +
-            "stock_name="+ String(req.query("stockName")) +
-        "JOIN "+
-            "stock s ON sn.stock_id = s.stock_id;"
-    ;
-
+    "SELECT "+
+        "s.ticker_symbol, "+
+        "s.name AS stock_name "+
+    "FROM "+
+        "stock_news sn, stock s "+
+    "WHERE "+
+        "s.name = '" + String(req.params.stockName) + "' "+
+        "AND  sn.stock_id = s.stock_id; ";
 
     try {
         const result = await pool.query(query);
         res.status(200).json(result.rows);
     } catch (err) {
         console.error('Error executing query', err.stack);
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500);
     }
 });
 
