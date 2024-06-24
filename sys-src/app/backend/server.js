87e1712028bbeb730f10e65ceebb166b18455920
Andreas Hecht <a.hecht1@oth-aw.de>
2024-06-24 15:49:00 +0200

    More Endpoints

--- sys-src/app/backend/server.js
+++ sys-src/app/backend/server.js
@@ -18,11 +18,12 @@ app.get('/api/sentiments',async (req, res) => {
     const query =
         "SELECT "+
             " s.name,"+
-            " AVG(sn.sentiment) as AVG_Sentiment "+
-        " FROM "+
-            " stock s, stock_news sn "+
-        " WHERE s.stock_id = sn.stock_id AND EXTRACT('week' FROM sn.pub_date) = EXTRACT('week' FROM now()) "+
-        " group by s.name; "
+            " s.ticker_symbol,"+
+            " AVG(sn.sentiment) AS AVG_Sentiment "+
+       " FROM "+
+            " stock s, stock_news sn " +
+       " WHERE s.stock_id = sn.stock_id AND sn.pub_date BETWEEN now() - INTERVAL '7 days' AND now() " +
+       " GROUP BY s.name , s.ticker_symbol; "
 
     try {
         const result = await pool.query(query);
@@ -33,20 +34,17 @@ app.get('/api/sentiments',async (req, res) => {
     }
 });
 
-// GET like /api/stockData?stockName=(insert Stock name)
+// GET like /api/stockData/(insert Stock name)
 app.get('/api/StockDataFor/:stockName', async (req, res) => {
     const query =
-    "SELECT "+
-        "s.name AS stock_name, "+
-        "s.ticker_symbol, "+
-        "sp.stock_price_val, "+
-        "sp.stock_price_time "+
-    "FROM "+
-        "stock s, stock_price sp "+
-    "WHERE "+
-        "s.stock_id = sp.stock_id "+
-        "AND EXTRACT('week' FROM sp.stock_price_time) = EXTRACT('week' FROM now()) "+
-        "AND s.name = '" + String(req.params.stockName) + "';"
+        "SELECT "+
+           " sp.stock_price_val, "+
+           " sp.stock_price_time "+
+        " FROM "+
+            "stock s, stock_price sp "+
+        " WHERE "+
+            "s.stock_id = sp.stock_id "+
+        " AND s.name = '" + String(req.params.stockName) + "'"
     ;
 
     try {
@@ -59,21 +57,22 @@ app.get('/api/StockDataFor/:stockName', async (req, res) => {
 });
 
 
-// GET like /api/sentimentSources?sourceName=(insert source name)&?stockName=(insert stock name)
+// GET like /api/sentimentSources/(insert source name)/(insert stock name)
 app.get('/api/sentimentSources/:stockName', async (req, res) => {
     const query =
-    "SELECT "+
-        "s.ticker_symbol, "+
-        "s.name AS stock_name, "+
-        "sn.title, "+
-        "sn.sentiment, "+
-        "sn.url "+
-    "FROM "+
-        "stock_news sn, stock s "+
-    "WHERE "+
-        "s.name = '" + String(req.params.stockName) + "' "+
-        "AND  sn.stock_id = s.stock_id "+
-        "AND sn.sentiment_exists ;"
+        "SELECT "+
+            " s.ticker_symbol,"+
+            " s.name AS stock_name,"+
+            " sn.title,"+
+            " sn.sentiment,"+
+            " sn.url AS source "+
+        " FROM "+
+            " stock_news sn, stock s "+
+        " WHERE "+
+            " s.name = '" + String(req.params.stockName) + "' "+
+            " AND  sn.stock_id = s.stock_id "+
+            " AND sn.sentiment_exists; "
+
 
     try {
         const result = await pool.query(query);
@@ -83,5 +82,52 @@ app.get('/api/sentimentSources/:stockName', async (req, res) => {
         res.status(500);
     }
 });
+
+// GET like /api/SentimentDataFor/stockName=(insert Stock name)
+app.get('/api/SentimentDataFor/:stockName', async (req, res) => {
+    const query =
+        " SELECT "+
+            " s.name, "+
+            " s.ticker_symbol, "+
+            " AVG(sn.sentiment) AS AVG_Sentiment "+
+        " FROM "+
+            " stock s, stock_news sn "+
+        " WHERE s.stock_id = sn.stock_id AND sn.pub_date BETWEEN now() - INTERVAL '7 days' AND now() AND s.name = '" + String(req.params.stockName) + "' "+
+        " GROUP BY s.ticker_symbol, s.name; "
+    ;
+
+    try {
+        const result = await pool.query(query);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error('Error executing query', err.stack);
+        res.status(500);
+    }
+});
+
+// GET like /api/ArticlesBySourceFor/(insert Stock name)
+app.get('/api/ArticlesBySourceFor/:stockName', async (req, res) => {
+    const query =
+        "SELECT "+
+            "s.ticker_symbol, "+
+            "sn.source_url, "+
+            "COUNT(sn.url) AS articles "+
+        "FROM "+
+            "stock_news sn, stock s "+
+        "WHERE "+
+            "s.name = '" + String(req.params.stockName) + "' "+
+            "AND sn.sentiment_exists AND sn.pub_date BETWEEN now() - INTERVAL '7 days' AND now() " +
+            "GROUP BY sn.source_url, s.ticker_symbol; "
+    ;
+
+    try {
+        const result = await pool.query(query);
+        res.status(200).json(result.rows);
+    } catch (err) {
+        console.error('Error executing query', err.stack);
+        res.status(500);
+    }
+});
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
