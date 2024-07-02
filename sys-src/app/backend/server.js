d96a0c6da19727582b7b2d1219ed754f97dd5aec
Andreas Hecht <a.hecht1@oth-aw.de>
2024-07-02 10:24:59 +0200

    Added start,end Date to queries

--- sys-src/app/backend/server.js
+++ sys-src/app/backend/server.js
@@ -219,7 +219,7 @@ app.get('/api/historicalDataInRange/:stockName/:startDate/:endDate',async (req, 
         `
 
     try {
-        const result = await pool.query(query, [String(req.params.stockName),]);
+        const result = await pool.query(query, [startDate, endDate ,String(req.params.stockName),]);
         res.status(200).json(result.rows);
     } catch (err) {
         console.error('Error executing query', err.stack);
